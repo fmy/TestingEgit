9b717cf848264119c515338932316ef33b23802b
Fumiya KARASAWA <krswfmy@gmail.com>
2011-10-05 21:28:23 +0900

    first commit

---
+++ src/com/sample/testingegit/TestingEgitActivity.java
@@ -0,0 +1,13 @@
+package com.sample.testingegit;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class TestingEgitActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
