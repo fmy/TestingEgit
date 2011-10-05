93d192f9a82c1e9b0e1cb8b9b02ecac609307866
Fumiya KARASAWA <krswfmy@gmail.com>
2011-10-05 21:42:06 +0900

    edit Test.java
    Change-Id: I39a0c1b283aae0a26ff8d202dc410806ec719ea7

--- src/com/sample/testingegit/Test.java
+++ src/com/sample/testingegit/Test.java
@@ -1,5 +1,17 @@
 package com.sample.testingegit;
 
-public class Test {
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.Button;
 
+public class Test  extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+        Button button = new Button(this);
+        button.setText("OK");
+        setContentView(button);
+    }
 }
